e55fa3ef5c9059f26a10c1a4552dcadf5dd139f2
Saumel-Das <saumel.spr@gmail.com>
2026-08-26 09:58:45 +0600

    added google O-auth client

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,5 +3,11 @@ import { betterAuth } from "better-auth";
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
-    }
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
 });
